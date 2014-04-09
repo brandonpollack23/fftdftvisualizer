3b3480b1cf645bd85a00d998cae792e1e088f660
Patrick Gauvin <gauvin@hcs.ufl.edu>
2014-04-08 22:52:09 -0400

    Fixed some goofiness on my part Corretn number of threads should be initialized and spawned now

--- src/ParallelDiscreteFT.java
+++ src/ParallelDiscreteFT.java
@@ -16,27 +16,22 @@ public class ParallelDiscreteFT extends SequentialDiscreteFT
 		int NUM_PROCS = Runtime.getRuntime().availableProcessors();
 		Thread[] thread = new Thread[NUM_PROCS];
 		SequentialDiscreteFT[] sequentialDiscreteFT = new SequentialDiscreteFT[NUM_PROCS];
-		// Queue<double[]>[] threadQueues = new Queue<double[]>[NUM_PROCS];
 		List<Queue<double[]>> threadQueues = new LinkedList<Queue<double[]>>();
 		
 		try
 		{
 			for (int i = 0, s = startPos; i < NUM_PROCS; i++, s += incSize)
 			{
-				// threadQueues[i] = new LinkedList<double[]>();
 				threadQueues.add(new LinkedList<double[]>());
-				// sequentialDiscreteFT[i] = new SequentialDiscreteFT(threadQueues[i], song, numBins, incSize * NUM_PROCS);
 				sequentialDiscreteFT[i] = new SequentialDiscreteFT(threadQueues.get(i), song, numBins, s,
 						incSize * NUM_PROCS);
 				thread[i] = new Thread(sequentialDiscreteFT[i]);
-				for (int j = 0; i < NUM_PROCS; i++)
-					thread[i].start();
-				
-				// TODO: Monitor the threadQueues and put the data into the main queue in order until all data is through
-				
-				for (int j = 0; j < NUM_PROCS; j++)
-					thread[j].join();
+				thread[i].start();
 			}
+			// TODO: Monitor the threadQueues and put the data into the main queue in order until all data is through
+			
+			for (int i = 0; i < NUM_PROCS; i++)
+				thread[i].join();
 		}
 		catch (Exception e)
 		{
